8d2e7afabf596366db629d47aff71ecc0231a414
Orion Black-Brown <kaylablack@D-69-91-142-96.dhcp4.washington.edu>
2017-01-25 11:18:09 -0800

    lab4

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,41 @@ interface IDomElement {
   print();
 }
 
+class DomElement implements IDomElement {
+  type:string;
+  text:string;
+  elementArray: IDomElement[];
+  constructor(type:string, text?:string) {
+    this.type = type;
+    this.text= text;
+    this.elementArray = [];
+  }
+
+  addElement(element: IDomElement) {
+    this.elementArray.push(element);
+  }
+
+  print() {
+    console.log(this.type);
+    console.log(this.text);
+    //
+  }
+}
+
+//leaf in tree
+class TextNode implements IDomElement {
+  text: string;
+  constructor(text:string) {
+    this.text = text;
+  }
+  print() {
+  console.log(this.text);
+  }
+}
+
+let p1 = new DomElement("<p>");
+let div = new DomElement("div");
+
+p1.addElement(new TextNode("textnode testing!"));
+
+div.addElement(p1);
